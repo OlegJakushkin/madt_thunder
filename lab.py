f7ef154e7bc1b96152f91a1713b88fbf73f75f93
nuvard <nikolskaya2000@yandex.ru>
2020-04-11 13:00:17 +0300

    empty tender

--- lab.py
+++ lab.py
@@ -8,7 +8,7 @@ def main():
     # create network nodes that will represent client and server
     for i in range(node_count):
         num = str(i+1)
-        thunder = net.create_node('thunder'+num, image='thunder',entrypoint='sh -c "while true; do wget -O - -T 3 $SERVER; sleep 1; python3 app.py; ./hello; done"')
+        thunder = net.create_node('thunder'+num, image='thunder')
         thunders.append(thunder)
     # create a local network that will connect all those nodes
     net.create_subnet('net', (thunders))
