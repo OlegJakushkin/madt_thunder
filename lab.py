614651636ba73284407e25352d93c02c1357f2f4
nuvard <nikolskaya2000@yandex.ru>
2020-04-24 16:53:50 +0300

    Added 4-node testnet working in MADT

--- lab.py
+++ lab.py
@@ -5,31 +5,34 @@ def main():
     net = Network('15.0.0.0/8')
     node_count = 4
     thunders = []
+    path = 'my:/home/nastya/magt/tutorials/my_1/build/tendermint:Z'
     # create network nodes that will represent client and server
     thunder1 = net.create_node('node1', 
                         privileged=True,
                         image="tendermint/localnode",
                         ports={'26656/tcp': 26656, '26657/tcp': 26657},
-                        volumes='./build:/tendermint:Z',
+                        #volumes=path,
                         environment={'ID':'0','LOG':'${LOG:-tendermint.log}'})
     thunder2 = net.create_node('node2', 
                         privileged=True,
                         image="tendermint/localnode",
-                        ports={'26659/tcp': 26659, '26660/tcp': 26660},
-                        volumes='./build:/tendermint:Z',
+                        ports={'26656/tcp': 26659, '26657/tcp': 26660},
+                        #volumes=path,
                         environment={'LOG':'${LOG:-tendermint.log}', 'ID':'1'})
     thunder3 = net.create_node('node3', 
                         privileged=True,
                         image="tendermint/localnode",
                         environment={'LOG':'${LOG:-tendermint.log}', 'ID':'2'},
-                        ports={'26661/tcp': 26661, '26662/tcp': 26662},
-                        volumes='./build:/tendermint:Z')
+                        ports={'26656/tcp': 26661, '26657/tcp': 26662},
+                        #volumes=path
+                        )
     thunder4 = net.create_node('node4', 
                         privileged=True,
                         image="tendermint/localnode",
                         environment={'LOG':'${LOG:-tendermint.log}', 'ID':'3'},
-                        ports={'26663/tcp': 26663, '26664/tcp': 26664},
-                        volumes='./build:/tendermint:Z')
+                        ports={'26656/tcp': 26663, '26657/tcp': 26664},
+                        #volumes=path
+                        )
     
     # create a local network that will connect all those nodes
     net.create_subnet('net', (thunder1,thunder2,thunder3,thunder4))
--- readme.md
+++ readme.md
@@ -13,14 +13,19 @@ sudo make && sudo make install
 
 sudo -HE env PYTHONPATH=$HOME/madt:$PYTHONPATH SSH_PWD=demo python3 madt_ui/main.py 80  
 ```
+2. Downoload this repo 
 
-2. Build images and start the lab
+Say you have this lab folder "my" in "tutorials"
+
+2. Build image for nodes (every node has 4 folders in it with initial data and config of all 4 starting  nodes)
 ```
 #open new terminal window
 cd ~/madt
-cd ./tutorials/basic
-docker pull inutano/wget
-docker build madt/nginx .
+cd ./tutorials/my/nodes
+make
+
+3. Start the lab
+cd ..
 python3 ./lab.py
 ```
 
@@ -28,4 +33,4 @@ python3 ./lab.py
 4. login as `demo:demo`
 5. Open lab ![image](https://user-images.githubusercontent.com/2915361/76143162-fe747180-606c-11ea-8b50-429b9067c62b.png)
 6. Observe graph ![image2](https://user-images.githubusercontent.com/2915361/76143179-2368e480-606d-11ea-8d11-8ce5d360884e.png)
-
+ in "tutorials" folder
