4d7711ab1e581f6a3ca777be1e854b0548d0a091
ddsvetlov <dankasvetlov@gmail.com>
2020-06-04 00:36:57 +0300

    gaga

--- lab.py
+++ lab.py
@@ -7,37 +7,37 @@ def main():
     thunders = []
     # path = 'my:/home/svetlov/Downloads/madt/tutorials/madt_thunder/build/tendermint:Z'
     # create network nodes that will represent client and server
-    # for x in range(1,4):
-	   #  thunders.append(net.create_node('node'+str(x), 
+    for x in range(1,16):
+	    thunders.append(net.create_node('node'+str(x), 
+                        privileged=True,
+                        image="tendermint/validator",
+                        ports={'26656/tcp': 26659+2*(x-1), '26657/tcp': 26660+2*(x-1)},
+                        environment={'ID':str(x-1),'LOG':'${LOG:-tendermint.log}'},
+                        # entrypoint="sleep "+str(int(x>4)*x*10)
+                        ))
+
+    # thunder1 = net.create_node('node1', 
     #                     privileged=True,
     #                     image="tendermint/validator",
-    #                     ports={'26656/tcp': 26659+2*(x-1), '26657/tcp': 26660+2*(x-1)},
-    #                     environment={'ID':str(x-1),'LOG':'${LOG:-tendermint.log}'},
-    #                     # entrypoint="sleep "+str(int(x>4)*x*10)
-    #                     ))
-
-    thunder1 = net.create_node('node1', 
-                        privileged=True,
-                        image="tendermint/validator",
-                        ports={'26656/tcp': 26656, '26657/tcp': 26657},
-                        environment={'ID':'0','LOG':'${LOG:-tendermint.log}'})
-    thunder2 = net.create_node('node2', 
-                        privileged=True,
-                        image="tendermint/validator",
-                        ports={'26656/tcp': 26659, '26657/tcp': 26660},
-                        environment={'LOG':'${LOG:-tendermint.log}', 'ID':'1'})
-    thunder3 = net.create_node('node3', 
-                        privileged=True,
-                        image="tendermint/validator",
-                        environment={'LOG':'${LOG:-tendermint.log}', 'ID':'2'},
-                        ports={'26656/tcp': 26661, '26657/tcp': 26662},
-                        )
-    thunder4 = net.create_node('node4', 
-                        privileged=True,
-                        image="tendermint/validator",
-                        environment={'LOG':'${LOG:-tendermint.log}', 'ID':'3'},
-                        ports={'26656/tcp': 26663, '26657/tcp': 26664},
-                        )
+    #                     ports={'26656/tcp': 26656, '26657/tcp': 26657},
+    #                     environment={'ID':'0','LOG':'${LOG:-tendermint.log}'})
+    # thunder2 = net.create_node('node2', 
+    #                     privileged=True,
+    #                     image="tendermint/validator",
+    #                     ports={'26656/tcp': 26659, '26657/tcp': 26660},
+    #                     environment={'LOG':'${LOG:-tendermint.log}', 'ID':'1'})
+    # thunder3 = net.create_node('node3', 
+    #                     privileged=True,
+    #                     image="tendermint/validator",
+    #                     environment={'LOG':'${LOG:-tendermint.log}', 'ID':'2'},
+    #                     ports={'26656/tcp': 26661, '26657/tcp': 26662},
+    #                     )
+    # thunder4 = net.create_node('node4', 
+    #                     privileged=True,
+    #                     image="tendermint/validator",
+    #                     environment={'LOG':'${LOG:-tendermint.log}', 'ID':'3'},
+    #                     ports={'26656/tcp': 26663, '26657/tcp': 26664},
+    #                     )
    
 
     # thunder5 = net.create_node('node5', 
@@ -50,8 +50,8 @@ def main():
     #                     )
     
     # create a local network that will connect all those nodes
-    # net.create_subnet('net', thunders)
-    net.create_subnet('net', (thunder1,thunder2,thunder3,thunder4))
+    net.create_subnet('net', thunders)
+    # net.create_subnet('net', (thunder1,thunder2,thunder3,thunder4))
     # distribute IP addresses
     net.configure(verbose=True)
 
